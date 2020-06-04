22f2eac6626b5cc476393f6bdeff10242e18ef68
tschelbs18 <ted.h.schelble@gmail.com>
2020-06-03 23:47:09 -0400

    Added travis + requirements files

--- fruitful/settings.py
+++ fruitful/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5qxtz!vs56i#sq$0m$u#nq3i*dl8l&a4(75=9e7cxnp0lc^#z%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
