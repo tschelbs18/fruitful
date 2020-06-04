7ba06bc74c21a5145092cf75805c1c4176a99edd
tschelbs18 <ted.h.schelble@gmail.com>
2020-06-04 10:19:43 -0400

    Made migrations, upgraded django

--- tasks/models.py
+++ tasks/models.py
@@ -6,19 +6,26 @@ class Fruit(models.Model):
     name = models.CharField(max_length=64)
 
 class StandardTask(models.Model):
-    pass
+    size = models.CharField(max_length=64)
+    name = models.CharField(max_length=2000)
 
 class CustomTask(models.Model):
-    pass
+    size = models.CharField(max_length=64)
+    name = models.CharField(max_length=2000)
 
 class UserTask(models.Model):
-    pass
+    size = models.CharField(max_length=64)
+    name = models.CharField(max_length=2000)
+    status = models.CharField(max_length=64)
 
 class StandardReward(models.Model):
-    pass
+    size = models.CharField(max_length=64)
+    name = models.CharField(max_length=1000)
 
 class CustomReward(models.Model):
-    pass
+    size = models.CharField(max_length=64)
+    name = models.CharField(max_length=1000)
 
 class UserReward(models.Model):
-    pass
+    size = models.CharField(max_length=64)
+    name = models.CharField(max_length=1000)
