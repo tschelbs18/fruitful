c18b8710f840ad320d0cb53ac421ba6bebe95841
tschelbs18 <ted.h.schelble@gmail.com>
2020-06-05 12:34:50 -0400

    Fixed host name

--- fruitful/settings.py
+++ fruitful/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fruitful6.herokuapp.com','afruitfulday.com']
+ALLOWED_HOSTS = ['fruitful6.herokuapp.com','www.afruitfulday.com']
 
 
 # Application definition
